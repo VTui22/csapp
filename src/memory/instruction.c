12bbb30cc0202b4d90763d400fcd90790ae6c353
root <root@DESKTOP-882QA8E.>
2025-01-07 23:05:49 +0800

    finish add function

--- src/memory/instruction.c
+++ src/memory/instruction.c
@@ -97,6 +97,11 @@ void init_handler_table()
     handler_table[mov_reg_reg] = &mov_reg_reg_handler;
     handler_table[call] = &call_handler;
     handler_table[add_reg_reg] = &add_reg_reg_handler;
+    handler_table[push_reg] = &push_reg_handler;
+    handler_table[pop_reg] = &pop_reg_handler;
+    handler_table[mov_reg_mem] = &mov_reg_mem_handler;
+    handler_table[mov_mem_reg] = &mov_mem_reg_handler;
+    handler_table[ret] = &ret_handler;
 }
 
 void mov_reg_reg_handler(uint64_t src, uint64_t dst)
@@ -106,6 +111,50 @@ void mov_reg_reg_handler(uint64_t src, uint64_t dst)
     *(uint64_t *)dst = *(uint64_t *)src;
     reg.rip = reg.rip + sizeof(inst_t);
 }
+
+void mov_reg_mem_handler(uint64_t src, uint64_t dst)
+{
+    // src: reg
+    // dst: mem virutal address
+    write64bits_dram(va2pa(dst), *(uint64_t*)src);
+    reg.rip = reg.rip + sizeof(inst_t);
+}
+
+void mov_mem_reg_handler(uint64_t src, uint64_t dst)
+{
+      // src: mem virutal address
+    // dst: reg
+    *(uint64_t *)dst = read64bits_dram(va2pa(src));
+    reg.rip = reg.rip + sizeof(inst_t);
+}
+
+void push_reg_handler(uint64_t src, uint64_t dst)
+{
+    // src: reg
+    // dst: empty
+    reg.rsp = reg.rsp - 0x8;
+    write64bits_dram(
+        va2pa(reg.rsp),
+        *(uint64_t *)src
+    );
+    reg.rip = reg.rip + sizeof(inst_t);
+}
+
+void pop_reg_handler(uint64_t src, uint64_t dst)
+{
+    // src: rbp: reg
+    *(uint64_t *)src = read64bits_dram(va2pa(reg.rsp));
+    reg.rsp += 8;
+    reg.rip = reg.rip + sizeof(inst_t);
+}
+
+void ret_handler(uint64_t src, uint64_t dst)
+{
+    uint64_t ret_addr = read64bits_dram(va2pa(reg.rsp));
+    reg.rsp += 8;
+    reg.rip = ret_addr;
+}
+
 
 void call_handler(uint64_t src, uint64_t dst)
 {
